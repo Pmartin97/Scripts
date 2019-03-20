cee992e2a963fec06a66124d4de5487e10585aef
Pmartin97 <33326210+Pmartin97@users.noreply.github.com>
2019-03-20 13:14:48 +0100

    Creacion del script
    Hay que modificar el nombre de los ficheros para que corresponda con lo necesario

---
+++ script_cura.py
@@ -0,0 +1,43 @@
+
+
+# Abrir fichero para lectura y escritura. El puntero se posiciona al principio del fichero
+fichero = open('datos', 'r+')
+
+
+# Abrir fichero de salida para lectura y escritura. El puntero se posiciona al principio del fichero
+ficherosalida = open('datossalida', 'rw+')
+
+
+# Cargamos todas las lineas en una lista y calculamos numlineas
+lineas = fichero.readlines()
+numlineas = len(lineas)
+numlayer = 1
+
+
+# Bucle for que calcula el nº de layer en el fichero
+for i in range(numlineas):
+  linea = str(lineas[i])
+  if ( linea[0:4] == "LAYER" )
+    numlayer += 1
+
+porcentaje = numlineas/100
+layer = 1
+p = 0
+nl = 0
+
+# Escritura del archivo final
+
+for i in numlineas:
+  linea = str(lineas[i])
+  nl += 1
+  if ( linea[0:4] == "LAYER)
+    ficherosalida.write("M117 L=", layer,"/",numlayer," P=",p ,"%/n")
+    layer += 1
+  ficherosalida.write(linea)
+  if ( nl == porcentaje )
+    ficherosalida.write("M117 L=", layer,"/",numlayer," P=",p ,"%/n")
+    nl = 1
+    p += 1
+  
+ficherosalida.close()
+fichero.close()
